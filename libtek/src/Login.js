1d4fa09f56e0519627d4879cea0558bab0101a9a
Earl Elorde <cpt.woof16@gmail.com>
2026-03-09 21:11:24 +0800

    Update Login.js

--- libtek/src/Login.js
+++ libtek/src/Login.js
@@ -40,7 +40,7 @@ const Login = () => {
     };
 
     try {
-      const response = await fetch("http://localhost:8080/api/user/login", {
+      const response = await fetch("https://libtekbackend-1.onrender.com/api/user/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(payload),
